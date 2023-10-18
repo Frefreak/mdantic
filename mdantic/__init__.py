3b045d2adc5c9769b3779475031474028de783bc
Trevor Wieland <trevor_wieland@mac.com>
2023-10-18 08:37:00 +0000

    Working changes, not yet test cases

---
+++ mdantic/__init__.py
@@ -0,0 +1,14 @@
+from .mdantic import (
+    Mdantic,
+    analyze,
+    Field,
+    get_related_enum,
+    get_enum_values,
+    get_related_enum_helper,
+    mk_struct,
+    fmt_tab,
+    MdanticPreprocessor,
+    makeExtension,
+)
+
+from .samples import SampleModel
--- setup.py
+++ setup.py
@@ -1,29 +1,30 @@
 from setuptools import setup
+
 setup(
-    name='markdown-mdantic',
-    version='1.3.6',
-    author='Xiangyu Zhu',
-    author_email='frefreak.zxy@gmail.com',
-    description='Python-Markdown extension for rendering pydantic BaseModel as table',
-    long_description=open('README.md').read(),
+    name="markdown-mdantic",
+    version="2.0.0",
+    author="Xiangyu Zhu",
+    author_email="frefreak.zxy@gmail.com",
+    description="Python-Markdown extension for rendering pydantic BaseModel as table",
+    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/frefreak/mdantic",
-    py_modules=['mdantic'],
-    install_requires=['markdown >= 3.0', 'tabulate', 'pydantic'],
+    py_modules=["mdantic"],
+    install_requires=["markdown >= 3.0", "tabulate", "pydantic"],
     classifiers=[
         # How mature is this project? Common values are
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Topic :: Text Processing :: Filters',
-        'Topic :: Text Processing :: Markup :: HTML',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Text Processing :: Filters",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
 )
